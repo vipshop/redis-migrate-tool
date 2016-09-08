306e6bff0f36828b13b1d1d8e4641b099527aa19
alessandrobologna <alessandro.bologna@gmail.com>
2016-09-08 11:44:40 -0400

    Support Elastic Cache
    - query AWS to find the primary endpoint and update the configuration
    file
    - added docker.md documentation for docker.

---
+++ entry-point.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -n "${CONFIG_URL}" ]
+then 
+	curl -s -o /migrate/rmt.conf ${CONFIG_URL}
+elif [ -n "${SOURCE}" ] 
+then
+	exec 6>&1           		 # Link file descriptor #6 with stdout.
+	exec > /migrate/rmt.conf     # stdout replaced with file.
+	echo -e "[source]\n${SOURCE}"
+	echo -e "[target]"
+	if [ -n "${REPLICATION_GROUP}" ]
+	then
+		echo -e "type: single\nservers:"
+		servers=$(aws elasticache describe-replication-groups --query "ReplicationGroups[?ReplicationGroupId==\`${REPLICATION_GROUP}\`].NodeGroups[].[PrimaryEndpoint.Address]" --output text)
+		for server in "${servers}"
+		do 
+			echo "-${server}:6379"
+	    done
+	elif [ -n "${TARGET}" ]
+	then
+		echo -e "${TARGET}"
+	fi
+	echo -e "[common]\nlisten: 0.0.0.0:8888"
+	exec 1>&6 6>&-      # Restore stdout and close file descriptor #6.
+fi
+cat /migrate/rmt.conf 
+/app/src/redis-migrate-tool -c /migrate/rmt.conf
+
