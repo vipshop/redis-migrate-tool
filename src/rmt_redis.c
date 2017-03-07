45bc40a2909f027fe592585df4cd994a6b522a65
deep011 <ieshen.zheng@163.com>
2017-03-07 14:30:48 +0800

    change 'EVAL/EVALSHA' command to not supported, and it will not sent to the target group

--- src/rmt_redis.c
+++ src/rmt_redis.c
@@ -2730,6 +2730,8 @@ redis_parse_req(struct msg *r)
 
                 if (str4icmp(m, 'e', 'v', 'a', 'l')) {
                     r->type = MSG_REQ_REDIS_EVAL;
+                    r->noforward = 1;
+                    r->not_support = 1;
                     break;
                 }
 
@@ -3047,6 +3049,8 @@ redis_parse_req(struct msg *r)
 
                 if (str7icmp(m, 'e', 'v', 'a', 'l', 's', 'h', 'a')) {
                     r->type = MSG_REQ_REDIS_EVALSHA;
+                    r->noforward = 1;
+                    r->not_support = 1;
                     break;
                 }
 
